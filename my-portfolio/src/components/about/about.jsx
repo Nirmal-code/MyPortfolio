f5c95b8f921234dfb93efdb61e914ae77788c4cd
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-08-24 13:33:15 -0500

    feature: about completed with interactive terminal
    Completed about with interactive terminal

--- my-portfolio/src/components/about/about.jsx
+++ my-portfolio/src/components/about/about.jsx
@@ -1,13 +1,86 @@
-import React,{ Component } from 'react';
-import Typed from 'react-typed';
+import React,{ Component, createRef  } from 'react';
+import MyPic from '../../images/my_picture.jpg';
+import Terminal from 'terminal-in-react';
 
 
 
 export default class About extends Component{
+
+    constructor(){
+        super();
+        this.state={
+            input:'',
+            output:"Welcome to my terminal. Enter 'help' to get started",
+        }
+        this.outputRef = createRef();
+
+
+    }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom = () => {
+        this.outputRef.current.scrollTop = this.outputRef.current.scrollHeight;
+    };
+
     render(){
         return(
-            <div id="about" className='h-fit text-white h-fit mb-20 w-[100vw] mt-[5vw]'>
+            <div id="about" className='h-[130vh] text-white h-fit mb-20 w-[100vw] mt-[5vw]'>
                 <h1 className='text-[3.5vw] ml-[5%]'>&lt;About Me / &gt;</h1>
+                <div className="grid grid-cols-[35vw_65vw] items-center justify-center pt-[3vw] ml-[20%]">
+                    <img src={MyPic} className="relative h-[35vw] rounded-xl"></img>
+                    <div ref={this.outputRef} className="w-[50vw] max-h-[35vw] h-[35vw] bg-black border-2 border-white overflow-scroll text-[1.5vw]">
+                        <div>
+                            <p dangerouslySetInnerHTML={{ __html: this.state.output }}></p>
+                        </div>
+                        <input
+                        className="text-white bg-transparent border-none w-[50vw]"
+                        type='text'
+                        value={this.state.input || 'C:\\NirmalC> '}
+                        onChange={e => {
+                            if (!e.target.value.includes('C:\\NirmalC> ')) {
+                                this.setState({ input: 'C:\\NirmalC> ' });
+                            } else {
+                                this.setState({ input: e.target.value });
+                            }
+                        }}
+                        onKeyDown={e=>{
+                            if (e.key==='Enter'){
+                                if (this.state.input!=""){
+                                    let new_output=""
+                                    new_output=this.state.output+"<br>"+this.state.input
+                                    switch(this.state.input){
+                                        case 'C:\\NirmalC> help':
+                                            new_output+="<p style='margin-left:5%;'>'des': My Description <br> 'gpa': Current GPA <br> 'courses': Relevant Courses <br> 'projects': Relevant Projects <br> 'experience': Relevant Work Experience <br> 'clear': Clear Terminal</p>"
+                                            break;
+                                        case 'C:\\NirmalC> des':
+                                            new_output+="<p style='margin-left:5%;'></div>Hello! I am a second-year Software Engineering student at McMaster University. I enjoy taking part in collaborative projects, especially those that I believe will help me make a positive impact on our society. I seek out opportunities that will allow me to develop these skills so that I may more effectively do this.</p>"
+                                            break;
+                                        case 'C:\\NirmalC> gpa':
+                                            new_output+="<p style='margin-left:5%;'>My current GPA is 3.98, or 11.9 on McMasters Scale. Use the 'courses' command to see my marks for the technical courses.</p>"
+                                            break;
+                                        case 'C:\\NirmalC> courses':
+                                            new_output+="<p style='margin-left:5%;'>Engineering Cornerstone Design Project, Grade:A+ <br> Object-Oriented Programming, Grade:A+ <br> Software Engineering Practice and Experience, Grade: A+ <br> Digital Systems and Interfacing, Grade: A+ <br> Data Structures and Algorithms, Grade: A+ <br> Software Design-Intro to Software Development, Grade: A+ <br> Computer Architecture, Grade: A</p>"
+                                            break;
+                                        case 'C:\\NirmalC> projects':
+                                            new_output+="<p style='margin-left:5%;'>I have completed 7+ projects. Checkout the Projects section of the website for more information </p>"
+                                            break;
+                                        case 'C:\\NirmalC> experience':
+                                            new_output+="<p style='margin-left:5%;'>I have over 2 years of relevant work experience. Checkout the Experiences section of the website for more information</p>"
+                                            break;
+                                        case 'C:\\NirmalC> clear':
+                                            this.state.output="Welcome to my terminal. Enter 'help' to get started"
+                                            new_output=this.state.output
+                                    }
+                                    this.setState({input:'',output:new_output})
+                                }
+                            }
+                        }
+                        }/>
+                    </div>
+                </div>
             </div>
         )
     }
--- my-portfolio/src/components/navbar/navbar.jsx
+++ my-portfolio/src/components/navbar/navbar.jsx
@@ -13,13 +13,13 @@ export default class navbar extends Component{
 
     render(){
         return(
-            <div className="text-[3rem] sm:text-[3rem] md:text-[3rem] lg:text-[3.5rem] xl:text-[4.5rem] fixed bottom-7 left-[10vw] p-3 rounded-full w-[80vw] bg-[#0284c7] bg-[#27272a] shadow-1xl shadow-[#71717a] border-2 border-[#a1a1aa]">
+            <div className="text-[3rem] sm:text-[3rem] md:text-[3rem] lg:text-[3.5rem] xl:text-[4.5rem] fixed bottom-7 left-[10vw] p-3 rounded-full w-[80vw] bg-slate-300 bg-white bg-opacity-50 shadow-1xl shadow-[#71717a]">
                 <div className="flex justify-center">
-                    <a href="#"><BiSolidHome className="p-3 text-black bg-white rounded-lg hover:scale-125 hover:bg-transparent hover:text-white hover:border-white border-2 border-black hover:cursor-pointer mr-2" /></a>
-                    <a href="#about"><HiOutlineInformationCircle className="p-3 text-black bg-white hover:scale-125 rounded-lg hover:bg-transparent hover:text-white hover:border-white border-2 border-black hover:cursor-pointer mr-2" /></a>
-                    <a><MdWorkHistory className="p-3 text-black bg-white rounded-lg hover:scale-125 hover:bg-transparent hover:text-white hover:cursor-pointer hover:border-white border-2 border-black mr-2 " /></a>
-                    <a><GoProjectSymlink className="p-3 text-black hover:scale-125 bg-white hover:bg-transparent hover:text-white hover:cursor-pointer hover:border-white border-2 border-black rounded-lg mr-2" /></a>
-                    <a><GiSkills className="p-3 text-black bg-white hover:scale-125 hover:bg-transparent hover:text-white hover:cursor-pointer hover:border-white border-2 border-black rounded-lg" /></a>
+                    <a href="#"><BiSolidHome className="p-3 text-white bg-black rounded-lg hover:scale-125 border-2 border-black hover:cursor-pointer mr-2.5" /></a>
+                    <a href="#about"><HiOutlineInformationCircle className="p-3 text-white bg-black rounded-lg hover:scale-125 border-2 border-black hover:cursor-pointer mr-2.5" /></a>
+                    <a><MdWorkHistory className="p-3 text-white bg-black rounded-lg hover:scale-125 border-2 border-black hover:cursor-pointer mr-2.5" /></a>
+                    <a><GoProjectSymlink className="p-3 text-white bg-black rounded-lg hover:scale-125 border-2 border-black hover:cursor-pointer mr-2.5" /></a>
+                    <a><GiSkills className="p-3 text-white bg-black rounded-lg hover:scale-125 border-2 border-black hover:cursor-pointer" /></a>
                 </div>
             </div>
         )
